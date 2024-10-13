124576a9c5b5c6e5c3d55e2e9bc0d42020291dce
Meir Ifrach <meir@justt.ai>
2024-10-13 22:34:37 +0300

    home link

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,19 +4,22 @@ const RotemHome = () => {
     return (
         <div className="min-h-screen flex flex-col justify-center items-center text-center bg-gradient-to-r from-primary-light to-secondary-light text-text">
             <div className="bg-white bg-opacity-80 p-8 rounded-lg shadow-xl max-w-2xl">
-                <h1 className="text-4xl md:text-6xl font-bold mb-6 text-primary font-secondary">רותם - אמנית איפור</h1>
+                <h1 className="text-4xl md:text-6xl font-bold mb-6 text-primary font-secondary">Rotem - Makeup Artist</h1>
                 <p className="text-xl md:text-2xl mb-8 text-text">
-                    מומחית באיפור מקצועי, עיצוב גבות וטיפול בשפם לנשים
+                    רותם יפרח להתחבר ליעוד שלך |
+                    מאפרת ומעצבת גבות
                 </p>
-                <div className="flex flex-wrap justify-center gap-4 mb-8">
-                    <span className="bg-secondary-light text-secondary px-3 py-1 rounded-full text-sm font-semibold">איפור כלות</span>
-                    <span className="bg-primary-light text-primary px-3 py-1 rounded-full text-sm font-semibold">איפור ערב</span>
+                <div className="flex flex-wrap justify-center gap-3 mb-8">
+                    <span className="bg-primary-light text-primary px-3 py-1 rounded-full text-sm font-semibold">איפור </span>
                     <span className="bg-blue-200 text-blue-800 px-3 py-1 rounded-full text-sm font-semibold">עיצוב גבות</span>
                     <span className="bg-green-200 text-green-800 px-3 py-1 rounded-full text-sm font-semibold">הסרת שיער</span>
                 </div>
-                <button className="bg-primary text-white px-8 py-3 rounded-full text-lg font-semibold hover:bg-secondary transition duration-300 transform hover:scale-105">
-                    קבעי תור עכשיו
-                </button>
+                <a href="https://wa.link/j96v07" target="_blank"  rel="noreferrer">
+                    <button
+                        className="bg-primary text-white px-8 py-3 rounded-full text-lg font-semibold hover:bg-secondary transition duration-300 transform hover:scale-105">
+                        קבעי תור עכשיו
+                    </button>
+                </a>
             </div>
         </div>
     );
