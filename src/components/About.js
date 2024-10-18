1697f71d704e7866ae42767b810916d2d7669e37
Meir Ifrach <meir@justt.ai>
2024-10-18 11:46:51 +0300

    add new image

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import rotemImage from '../img/IMG_9439.jpg'; // Import the image
+import rotemImage from '../img/rotem.jpg'; // Import the image
 
 const About = () => {
     return (
         <div className="container mx-auto px-4 py-16 bg-white text-center">
-            <h1 className="text-3xl md:text-4xl font-bold mb-10 text-center text-primary font-secondary text-pink-500">אודות רותם יפרח</h1>
+            <h1 className="text-3xl md:text-4xl font-bold mb-10 text-center text-primary font-secondary text-pink-500">אודות
+                רותם יפרח</h1>
             <div className="flex flex-col md:flex-row items-center">
                 <div className="md:w-1/2 mb-8 md:mb-0">
-                    <img src={rotemImage} alt="רותם יפרח - אמנית איפור" className="rounded-lg shadow-2xl transform hover:scale-105 transition duration-500" />
+                    <img src={rotemImage} alt="רותם יפרח - אמנית איפור"
+                         className="rounded-lg shadow-2xl transform hover:scale-105 transition duration-500"/>
                 </div>
                 <div className="md:w-1/2 md:pr-12">
                     <p className="text-lg mb-6 leading-relaxed text-text">
-                        שלום, אני רותם יפרח - אמנית איפור מקצועית עם ניסיון של למעלה מ-10 שנים בתחום. אני מאמינה שאיפור הוא אומנות שמשלבת יצירתיות, טכניקה וקסם.
+                        שלום, אני רותם יפרח - אמנית איפור מקצועית עם ניסיון של למעלה מ-10 שנים בתחום. אני מאמינה שאיפור
+                        הוא אומנות שמשלבת יצירתיות, טכניקה וקסם.
                     </p>
                     <p className="text-lg mb-6 leading-relaxed text-text">
                         אני נהנית להשתמש בכישורים שלי כדי לעזור לנשים להרגיש יפות ובטוחות בעצמן, לא משנה מה האירוע.
