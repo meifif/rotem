0ed1e522a7b4e53e880835840fe2128e919c5419
Meir Ifrach <meir@justt.ai>
2024-10-19 09:13:27 +0300

    change links to new site

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -8,28 +8,28 @@ const SEO = () => {
                   content="רותם יפרח, אמנית איפור מקצועית בחולון. מתמחה באיפור כלות, איפור ערב, עיצוב גבות והסרת שיער. הזמינו תור עכשיו!"/>
             <meta name="keywords"
                   content="איפור, אמנית איפור, איפור כלות, איפור ערב, עיצוב גבות, הסרת שיער, חולון"/>
-            <link rel="canonical" href="https://meifif.github.io/rotem/"/>
+            <link rel="canonical" href="https://rotem-ifrach.co/"/>
             <meta property="og:title" content="רותם יפרח - אמנית איפור מקצועית בחולון"/>
             <meta property="og:description"
                   content="שירותי איפור מקצועיים לכלות, אירועים, ועיצוב גבות. הזמינו תור עם רותם יפרח, אמנית איפור מובילה בחולון."/>
             <meta property="og:image"
-                  content="https://meifif.github.io/rotem/static/media/IMG_9439.5f23f88cb4c65c15f1f8.jpg"/>
-            <meta property="og:url" content="https://meifif.github.io/rotem/"/>
+                  content="https://rotem-ifrach.co/static/media/IMG_9439.5f23f88cb4c65c15f1f8.jpg"/>
+            <meta property="og:url" content="https://rotem-ifrach.co/"/>
             <meta property="og:type" content="website"/>
             <meta name="twitter:card" content="summary_large_image"/>
             <meta name="twitter:title" content="רותם יפרח - אמנית איפור מקצועית בחולון"/>
             <meta name="twitter:description"
                   content="שירותי איפור מקצועיים לכלות, אירועים, ועיצוב גבות. הזמינו תור עם רותם יפרח, אמנית איפור מובילה בחולון."/>
             <meta name="twitter:image"
-                  content="https://meifif.github.io/rotem/static/media/IMG_9439.5f23f88cb4c65c15f1f8.jpg"/>
+                  content="https://rotem-ifrach.co/static/media/IMG_9439.5f23f88cb4c65c15f1f8.jpg"/>
             <script type="application/ld+json">
                 {`
                 {
                   "@context": "https://schema.org",
                   "@type": "BeautySalon",
                   "name": "רותם יפרח - אמנית איפור",
-                  "image": "https://meifif.github.io/rotem/static/media/IMG_9439.5f23f88cb4c65c15f1f8.jpg",
-                  "url": "https://meifif.github.io/rotem/",
+                  "image": "https://rotem-ifrach.co/static/media/IMG_9439.5f23f88cb4c65c15f1f8.jpg",
+                  "url": "https://rotem-ifrach.co/",
                   "telephone": "053-2509989",
                   "address": {
                     "@type": "PostalAddress",
